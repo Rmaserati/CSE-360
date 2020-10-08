79efeb3d2045063cebe3996b2c86dc90f6107145
Rishabh Marisetti <Rishabh.marisetti@gmail.com>
2020-10-07 18:57:25 -0700

    Version 1.0

--- CSE 360 Assignment 1/src/cse360assignment02/AddingMachine.java
+++ CSE 360 Assignment 1/src/cse360assignment02/AddingMachine.java
@@ -1,36 +1,50 @@
+/**
+ * The AddingMachine program does calculations which a calculator could do.
+ * 
+ *@Rishabh Marisetti
+ *@version 1.0
+ *@since 2020-09-07
+ */
+
+
 package cse360assignment02;
 
 public class AddingMachine {
   private int total;
-  private String history = "0";
-  
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
   
+  /**
+   * This method gets the total.
+   * @return NOTHING
+   */
+  
   public int getTotal () {
-    return total;
-    
+    return 0;
   }
+  /**
+   * This method does the add function.
+   * @param value This is the only parameter to AddingMachine.
+   * @return NOTHING
+   */
   
   public void add (int value) {
-	  total = total + value;
-	  history += " + " + value;
   }
+  /**
+   * This method does the subtract function.
+   * @param value This is the only parameter to AddingMachine.
+   * @return NOTHING
+   */
 
   public void subtract (int value) {
-	  total = total - value;
-	  history += " - " + value;
   }
 
   public String toString () {
-    return history;
+    return "";
   }
 
   public void clear() {
-	  total = 0;
-	  history = "0";
   }
-  
 }
